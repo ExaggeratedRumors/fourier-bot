27c1c2bbaeb0bbff08b407d8eb203e885beade06
exaggeratedrumors <exaggeratedrumors@gmail.com>
2023-08-25 14:19:57 +0200

    Getter and Uploader implementation.

--- src/main/kotlin/com/discord/fourierbot/commands/Getter.kt
+++ src/main/kotlin/com/discord/fourierbot/commands/Getter.kt
@@ -1,12 +1,8 @@
 package com.discord.fourierbot.commands
 
 import com.discord.fourierbot.encryption.CodesManager
-import com.discord.fourierbot.processing.ImagesManager
-import com.discord.fourierbot.utils.IMAGES_PATH
 import com.discord.fourierbot.utils.Resources
 import net.dv8tion.jda.api.entities.Message
-import net.dv8tion.jda.api.utils.FileUpload
-import java.io.File
 
 class Getter : Command() {
     override fun execute(message: Message) {
@@ -60,24 +56,5 @@ class Getter : Command() {
             .complete()
             .sendMessage(alert)
             .queue()
-    }
-
-    private fun sendImageToAuthor(message: Message) {
-        val userCode = CodesManager().getUserCodeById(message.author.idLong)
-            ?: createNewUserEntity(message.author.idLong, message.author.name)
-
-        message
-            .author
-            .openPrivateChannel()
-            .complete()
-            .sendMessage("Zrzut aktualnie analizowanych przedmiotów:")
-            .addFiles(FileUpload.fromData(File("$IMAGES_PATH/$userCode.png")))
-            .queue()
-    }
-
-    private fun createNewUserEntity(userId: Long, userName: String): String {
-        val code = CodesManager().addEntity(userId, userName)
-        ImagesManager().createImageCopy(code)
-        return code
     }
 }
